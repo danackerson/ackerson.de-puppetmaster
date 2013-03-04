e35303616f0fbf648f6fcabf3929870ad536da32
Dan Ackerson <dan@ackerson.de>
2013-03-04 17:16:07 +0100

    git clone connectreporting

---
+++ modules/reporting-app/manifests/init.pp
@@ -0,0 +1,14 @@
+# git clone the ConnectReporting app and 
+# setup the necessary cronjobs/scripts for daily data import
+class reporting-app {
+  file { '/root/dev':
+    ensure => directory
+  }
+
+  exec { 'git clone connectreporting':
+    cwd     => '/root/dev',
+    command => '/usr/bin/git clone git@github.com:NemetschekAllplan/ConnectReporting.git connectreporting',
+    unless  => '/usr/bin/test -d /root/dev/connectreporting/.git',
+    require => File['/root/dev'],
+  }
+}
