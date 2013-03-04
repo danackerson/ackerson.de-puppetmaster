d96577fd8dd1f61f4a88ca33f099647b71712bda
Dan Ackerson <dan@ackerson.de>
2013-03-04 12:49:18 +0100

    added ssl-cert and build-essential dependencies

--- modules/setenv/manifests/init.pp
+++ modules/setenv/manifests/init.pp
@@ -1,24 +1,18 @@
+# base environment class to install most common enhancers,
+# setup vi as default editor, etc.
 class setenv {
 
-  package { sudo: ensure => latest }
+  $enhancers = [  'sudo', 'htop', 'unzip', 'git-core', 'ssl-cert',
+                  'vim', 'fail2ban', 'build-essential' ]
+  package { $enhancers: ensure => 'installed' }
 
-  package { htop : ensure => latest }
-  
-  package { unzip : ensure => latest }
-  
-  package { git-core: ensure => latest }
-  
-  package { vim : ensure => latest }
-  
-  package { fail2ban : ensure => latest }
-
-  exec { "/usr/sbin/update-alternatives --set editor /usr/bin/vim.basic":
-    unless => "/usr/bin/test /etc/alternatives/editor -ef /usr/bin/vim.basic"
+  exec { '/usr/sbin/update-alternatives --set editor /usr/bin/vim.basic':
+    unless => '/usr/bin/test /etc/alternatives/editor -ef /usr/bin/vim.basic'
   }
-  
-  file { "/etc/environment":
-    ensure => present,
-    content => template("setenv/etc/environment.erb"),
+
+  file { '/etc/environment':
+    ensure  => present,
+    content => template('setenv/etc/environment.erb'),
   }
 
 }
