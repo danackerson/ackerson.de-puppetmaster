fb17d71d1306b0da8d67fd416184a90cf513ceea
Dan Ackerson <dan@ackerson.de>
2013-03-04 13:14:46 +0100

    add apt-get update for build-essential pkg

--- modules/setenv/manifests/init.pp
+++ modules/setenv/manifests/init.pp
@@ -1,14 +1,18 @@
 # base environment class to install most common enhancers,
 # setup vi as default editor, etc.
 class setenv {
-  package { 'sudo':             ensure => 'installed' }
-  package { 'htop':             ensure => 'installed' }
-  package { 'unzip':            ensure => 'installed' }
-  package { 'vim' :             ensure => 'installed' }
-  package { 'git-core':         ensure => 'installed' }
-  package { 'ssl-cert':         ensure => 'installed' }
-  package { 'fail2ban':         ensure => 'installed' }
-  package { 'build-essential':  ensure => 'installed' }
+  package { 'sudo':     ensure => installed }
+  package { 'htop':     ensure => installed }
+  package { 'unzip':    ensure => installed }
+  package { 'vim' :     ensure => installed }
+  package { 'git-core': ensure => installed }
+  package { 'ssl-cert': ensure => installed }
+  package { 'fail2ban': ensure => installed }
+
+  package { 'build-essential':
+    ensure  => latest,
+    require => Exec['apt-get update']
+  }
 
   exec { '/usr/sbin/update-alternatives --set editor /usr/bin/vim.basic':
     unless  => '/usr/bin/test /etc/alternatives/editor -ef /usr/bin/vim.basic',
