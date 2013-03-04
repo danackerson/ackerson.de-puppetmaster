72f81130ea1862941b4aaf4e59e2ae5da83250b8
Dan Ackerson <dan@ackerson.de>
2013-03-04 12:57:49 +0100

    fancy array way failed

--- modules/setenv/manifests/init.pp
+++ modules/setenv/manifests/init.pp
@@ -1,13 +1,18 @@
 # base environment class to install most common enhancers,
 # setup vi as default editor, etc.
 class setenv {
-
-  $enhancers = [  'sudo', 'htop', 'unzip', 'git-core', 'ssl-cert',
-                  'vim', 'fail2ban', 'build-essential' ]
-  package { $enhancers: ensure => 'installed' }
+  package { 'sudo':             ensure => 'installed' }
+  package { 'htop':             ensure => 'installed' }
+  package { 'unzip':            ensure => 'installed' }
+  package { 'vim' :             ensure => 'installed' }
+  package { 'git-core':         ensure => 'installed' }
+  package { 'ssl-cert':         ensure => 'installed' }
+  package { 'fail2ban':         ensure => 'installed' }
+  package { 'build-essential':  ensure => 'installed' }
 
   exec { '/usr/sbin/update-alternatives --set editor /usr/bin/vim.basic':
-    unless => '/usr/bin/test /etc/alternatives/editor -ef /usr/bin/vim.basic'
+    unless  => '/usr/bin/test /etc/alternatives/editor -ef /usr/bin/vim.basic',
+    require => Package['vim']
   }
 
   file { '/etc/environment':
