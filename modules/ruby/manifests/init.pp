6b1ed7ec6942c995113570649194192ebf3f2169
Dan Ackerson <dan@ackerson.de>
2013-03-05 17:23:05 +0100

    replace virtual pkgs with real

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -1,6 +1,6 @@
 # base ruby setup and gem bundler
 class ruby {
-  package { ['ruby1.9.1-full', 'rubygems', 'libxslt-dev', 'libxml2-dev', 'zlib1g-dev', 'libssl-dev', 'libyaml-dev', 'libsqlite3-dev', 'autoconf', 'libc6-dev', 'ncurses-dev']: ensure => installed }
+  package { ['ruby1.9.1-full', 'rubygems', 'libxslt1-dev', 'libxml2-dev', 'zlib1g-dev', 'libssl-dev', 'libyaml-dev', 'libsqlite3-dev', 'autoconf', 'libc6-dev', 'libncurses5-dev']: ensure => installed }
 
   ruby::gem { 'bundle':
     package => 'bundle'
