9c0fc2fc7e0a643ee0ce0b76701a3ecaa2da2da2
Dan Ackerson <dan@ackerson.de>
2013-03-05 10:01:55 +0100

    incl. syntax

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -2,7 +2,7 @@
 class ruby {
   package { ['ruby1.9.1-full', 'rubygems', 'libxslt1-dev', 'libxml2-dev']: ensure => installed }
 
-  ruby::gem { 'bundle':
+  gem { 'bundle':
     package => 'bundle'
   }
 }
