e090556eacfe95c8b71ca89a57a881289e251da8
Dan Ackerson <dan@ackerson.de>
2013-03-05 10:22:17 +0100

    puppet 2.7 correct

--- modules/ruby/manifests/gem.pp
+++ modules/ruby/manifests/gem.pp
@@ -1,8 +1,8 @@
-# gem bundler definition
-class ruby::gem($package, $version = '>= 0') {
-  exec { "gem-install--${package}-${version}":
+# and gem bundler
+define ruby::gem($ruby::package, $ruby::version = '>= 0') {
+  exec { "gem-install--${ruby::package}-${ruby::version}":
     require => Package['ruby1.9.1-full','rubygems','libxslt1-dev','libxml2-dev'],
-    command => "/usr/bin/gem install ${package} --version ${version}",
-    unless  => "/usr/bin/gem list | /bin/grep ${package}",
+    command => "/usr/bin/gem install ${ruby::package} --version ${ruby::version}",
+    unless  => "/usr/bin/gem list | /bin/grep ${ruby::package}",
   }
 }
--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -1,8 +1,7 @@
-# get ruby setup and gem bundler
+# base ruby setup and gem bundler
 class ruby {
   package { ['ruby1.9.1-full', 'rubygems', 'libxslt1-dev', 'libxml2-dev']: ensure => installed }
 
-  include ruby::gem
   ruby::gem { 'bundle':
     package => 'bundle'
   }
