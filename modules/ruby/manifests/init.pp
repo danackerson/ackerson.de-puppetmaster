25b021b51f9bf8147e4eb2af2260f422d0eefce2
Dan Ackerson <dan@ackerson.de>
2013-03-05 10:06:51 +0100

    include ruby gem another way

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -2,6 +2,7 @@
 class ruby {
   package { ['ruby1.9.1-full', 'rubygems', 'libxslt1-dev', 'libxml2-dev']: ensure => installed }
 
+  include ruby::gem
   ruby::gem { 'bundle':
     package => 'bundle'
   }
