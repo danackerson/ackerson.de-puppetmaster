a944914c52f7987e42e6bde7bfae596162ad30bb
Dan Ackerson <dan@ackerson.de>
2013-03-05 10:24:10 +0100

    syntax fix

--- modules/ruby/manifests/gem.pp
+++ modules/ruby/manifests/gem.pp
@@ -1,8 +1,8 @@
 # and gem bundler
-define ruby::gem($ruby::package, $ruby::version = '>= 0') {
-  exec { "gem-install--${ruby::package}-${ruby::version}":
+define ruby::gem($package, $version = '>= 0') {
+  exec { "gem-install--${package}-${version}":
     require => Package['ruby1.9.1-full','rubygems','libxslt1-dev','libxml2-dev'],
-    command => "/usr/bin/gem install ${ruby::package} --version ${ruby::version}",
-    unless  => "/usr/bin/gem list | /bin/grep ${ruby::package}",
+    command => "/usr/bin/gem install ${package} --version ${version}",
+    unless  => "/usr/bin/gem list | /bin/grep ${package}",
   }
 }
