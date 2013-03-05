23beedce3d494595770544fbbf6f969d7fb2124f
Dan Ackerson <dan@ackerson.de>
2013-03-05 10:50:34 +0100

    version in ticks

--- modules/ruby/manifests/gem.pp
+++ modules/ruby/manifests/gem.pp
@@ -1,5 +1,5 @@
 # and gem bundler
-define ruby::gem($package, $version = '>= 0') {
+define ruby::gem($package, $version = "'>= 0'") {
   exec { "gem-install--${package}-${version}":
     require => Package['ruby1.9.1-full','rubygems','libxslt1-dev','libxml2-dev'],
     command => "/usr/bin/gem install ${package} --version ${version}",
